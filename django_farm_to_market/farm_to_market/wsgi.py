12a28f9891bf2e6e00c47c9aa6caf8a6f838fa4f
EPHRANCINA <ephrancinabeyanga@gmail.com>
2025-05-23 21:23:20 +0300

    Temporarily modify wsgi.py for basic WSGI check

--- django_farm_to_market/farm_to_market/wsgi.py
+++ django_farm_to_market/farm_to_market/wsgi.py
@@ -1,7 +1,18 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farm_to_market.settings')
 
-application = get_wsgi_application() 
+application = get_wsgi_application()
+
+def simple_app(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+    start_response(status, headers)
+    return [b"Basic WSGI check passed!"]
+
+# Temporarily set the application to our simple test app
+# IMPORTANT: Remember to revert this after testing!
+application = simple_app 
